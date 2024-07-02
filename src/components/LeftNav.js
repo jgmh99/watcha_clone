41abf6f77ee4a2fc3eb30334c78a8b193adc684d
alsgur8780@daum.net <alsgur8780@daum.net>
2024-07-02 11:52:01 +0900

    test

--- src/components/LeftNav.js
+++ src/components/LeftNav.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import { Nav } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useLocation } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 const LeftNav = () => {
 
   const location = useLocation();
@@ -17,15 +16,15 @@ const LeftNav = () => {
   
   return (
     <div className='leftNavBar ps-3 d-none d-md-block d-lg-block d-xl-block '>
-        <Nav defaultActiveKey="/" className="flex-column">
-            <NavLink  style={{color : 'white'}} href="/subscribe">구독</NavLink>
-            <NavLink  style={{color : 'white'}} href="/individualPurchase">개별구매</NavLink>
-            <NavLink  style={{color : 'white'}} href="/webtoon">웹툰</NavLink>
+        <Nav className="flex-column">
+            <NavLink  style={{color : 'white'}} to="/subscribe">구독</NavLink>
+            <NavLink  style={{color : 'white'}} to="/individualPurchase">개별구매</NavLink>
+            <NavLink  style={{color : 'white'}} to="/webtoon">웹툰</NavLink>
             <div className='hrLine'/>
-            <NavLink  style={{color : 'white'}} href="/party">파티</NavLink>
-            <NavLink  style={{color : 'white'}} href="/finder">찾기</NavLink>
-            <NavLink  style={{color : 'white'}} href="/rating">평가</NavLink>
-            <NavLink  style={{color : 'white'}} href="/locker">보관함</NavLink>
+            <NavLink  style={{color : 'white'}} to="/party">파티</NavLink>
+            <NavLink  style={{color : 'white'}} to="/finder">찾기</NavLink>
+            <NavLink  style={{color : 'white'}} to="/rating">평가</NavLink>
+            <NavLink  style={{color : 'white'}} to="/locker">보관함</NavLink>
         </Nav>
   </div>
   )
