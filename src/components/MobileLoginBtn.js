14a6ff81b65334ae759c497e3f11d5ac3108b0c1
alsgur8780@daum.net <alsgur8780@daum.net>
2024-07-02 12:55:12 +0900

    모바일 UI수정

--- src/components/MobileLoginBtn.js
+++ src/components/MobileLoginBtn.js
@@ -5,13 +5,13 @@ import {faUser} from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 const MobileLoginBtn = () => {
     return (
-        <div style={{color:'black'}}>
-            <DropdownButton className='mobile_btn d-flex align-items-center' id="dropdown-basic-button" title={<FontAwesomeIcon icon={faUser} />} variant="dark">
-                <Dropdown.Item>
-                    <Link to="/login">로그인</Link>
+        <div>
+            <DropdownButton className='mobile_btn d-flex align-items-center' id="dropdown-button-dark-example2" title={<FontAwesomeIcon icon={faUser} />}variant="secondary" data-bs-theme="dark">
+                <Dropdown.Item as={Link} to="/login">
+                    로그인
                 </Dropdown.Item>
-                <Dropdown.Item>
-                    <Link to="/join">회원가입</Link>
+                <Dropdown.Item as={Link} to="/join">
+                    회원가입
                 </Dropdown.Item>
             </DropdownButton>
         </div>
