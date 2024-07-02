b39be1b0e8ffbb33970a99eb7768e22dc3d03e8e
alsgur8780@daum.net <alsgur8780@daum.net>
2024-07-02 14:04:16 +0900

    찐수정

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -32,9 +32,11 @@ const Login = () => {
   // 배경 변경
   useEffect(() => {
     // 페이지 로드 시 배경 이미지 설정
+    document.body.style.height = '100vh'
     document.body.style.backgroundImage = 'linear-gradient( rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7) ),url(https://image.tmdb.org/t/p/original/fMtgGnPtT2X5OV9ctiZaIlbIBOu.jpg)';
     document.body.style.backgroundSize = 'cover';
     document.body.style.backgroundPosition = '50% 40%';
+    document.body.style.objectFit = 'cover';
     document.querySelector('.topNav').style.backgroundColor = 'transparent';
     // 페이지 언마운트 시 배경 이미지 초기화
     return () => {
