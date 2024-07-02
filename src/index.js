1b7c68512318b57c7b45fb27ee695a4f5a87b55c
alsgur8780@daum.net <alsgur8780@daum.net>
2024-07-02 11:13:03 +0900

    #4

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-// import {BrowserRouter} from 'react-router-dom';
-import { HashRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
+// import { HashRouter} from 'react-router-dom';
 import store from './redux/MovieStore'
 import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
