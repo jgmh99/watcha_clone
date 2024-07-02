313bb2db0cf0438225d77393e5cb70808849231c
alsgur8780@daum.net <alsgur8780@daum.net>
2024-07-02 12:13:07 +0900

    test

--- src/components/MobileLoginBtn.js
+++ src/components/MobileLoginBtn.js
@@ -2,12 +2,17 @@ import React from 'react'
 import { Dropdown, DropdownButton } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faUser} from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 const MobileLoginBtn = () => {
     return (
         <div>
             <DropdownButton className='mobile_btn d-flex align-items-center' id="dropdown-basic-button" title={<FontAwesomeIcon icon={faUser} />} variant="dark">
-                <Dropdown.Item to="/login">로그인</Dropdown.Item>
-                <Dropdown.Item to="/join">회원가입</Dropdown.Item>
+                <Dropdown.Item>
+                    <Link to="/login">로그인</Link>
+                </Dropdown.Item>
+                <Dropdown.Item>
+                    <Link to="/join">회원가입</Link>
+                </Dropdown.Item>
             </DropdownButton>
         </div>
     )
